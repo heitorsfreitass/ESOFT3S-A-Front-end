3233015cdcb5675ca14b75c362d3b8c1ba6b4fb9
Heitor Freitas <heitor.freitas@crmall.com.br>
2025-05-21 14:06:13 -0300

    Importing global.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css' // Importando aqui para usar somente um arquivo de estilo, sem repetir código
 
 createApp(App).mount('#app')
